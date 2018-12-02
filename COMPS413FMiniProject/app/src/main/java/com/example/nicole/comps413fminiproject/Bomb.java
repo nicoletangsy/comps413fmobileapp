e358f2a953c9a8419f26c70b000fd1e3720e4b76
Nicole Tang <nicoletangsy@gmail.com>
2018-12-02 23:28:36 +0800

    add buff item bomb (unfinished)

--- COMPS413FMiniProject/app/src/main/java/com/example/nicole/comps413fminiproject/Bomb.java
+++ COMPS413FMiniProject/app/src/main/java/com/example/nicole/comps413fminiproject/Bomb.java
@@ -1,4 +1,49 @@
 package com.example.nicole.comps413fminiproject;
 
+import android.content.Context;
+import android.graphics.Canvas;
+
+/** The obstacles. */
 public class Bomb {
+    private Obstacle [] bomb = new Obstacle[1];
+
+
+    /** Constructor. */
+    public Bomb (Context context) {
+        float x = MainView.arenaWidth;
+        int y = MainView.arenaHeight;
+
+        bomb[0] = new Obstacle(context, R.drawable.bomb2);
+        bomb[0].setPosition(x, y);
+    }
+
+    /** Move the obstacles. */
+    public void move() {
+        for (Obstacle obstacle:bomb) {
+            obstacle.move();
+        }
+    }
+
+    /** Draw the obstacles. */
+    public void drawOn(Canvas canvas) {
+        for (Obstacle obstacle:bomb) {
+            obstacle.drawOn(canvas);
+        }
+    }
+
+    /** Inner class, represents an individual obstacle, i.e., one pipe. */
+    class Obstacle extends Sprite {
+
+        /** Constructor. */
+        public Obstacle(Context context, int resId) {
+            drawable = context.getResources().getDrawable(resId);
+        }
+
+        @Override
+        /** Move the obstacle. */
+        public void move() {
+                updateBounds();
+        }
+
+    }
 }
